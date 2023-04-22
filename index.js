8056532c238ce5f8cbabcdfb81fbe9308b4054f2
rscavuslu <samet.cavuslu@hotmail.com>
2023-04-22 22:17:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id','victory');
+
+const text = document.createTextNode('Rıfat is the champion');
+newHeader.appendChild(text);
